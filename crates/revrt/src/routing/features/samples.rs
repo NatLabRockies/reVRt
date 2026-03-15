2ce4d4b50c35d8a12acf82eaed25d0f44ce3c67d
Gui Castelao <guilherme@castelao.net>
2026-03-14 19:53:24 -0600

    cfg: Updating random trait on latest Rust

--- crates/revrt/src/routing/features/samples.rs
+++ crates/revrt/src/routing/features/samples.rs
@@ -5,7 +5,7 @@ use std::sync::Arc;
 
 use ndarray::Array2;
 use object_store::local::LocalFileSystem;
-use rand::Rng;
+use rand::RngExt;
 use tempfile::TempDir;
 use zarrs::array::{ArrayBuilder, DataType, FillValue};
 use zarrs::array_subset::ArraySubset;
