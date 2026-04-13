90656b3e8b351325172ab66e4a5c99be67a01bb0
github-action-benchmark <github@users.noreply.github.com>
2026-04-13 22:30:39 +0000

    add Rust Benchmark (cargo) benchmark result for 7ae322fc2b8886841cf8f8f3abe0a9bd1e034cea

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1776119419586,
+  "lastUpdate": 1776119439256,
   "repoUrl": "https://github.com/NatLabRockies/reVRt",
   "entries": {
     "Rust Benchmark": [
@@ -8315,6 +8315,90 @@ window.BENCHMARK_DATA = {
             "name": "distance/10",
             "value": 240196944,
             "range": "± 2038948",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ppinchuk@nrel.gov",
+            "name": "Paul Pinchuk",
+            "username": "ppinchuk"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7ae322fc2b8886841cf8f8f3abe0a9bd1e034cea",
+          "message": "Add A-star support (#251)\n\n* Add astar helper functions\n\n* Add support for astar\n\n* Update function call\n\n* Add astar tests\n\n* Update docstrings\n\n* Update docstring\n\n* Add python tests for astar",
+          "timestamp": "2026-04-13T16:09:35-06:00",
+          "tree_id": "b072c455d3ed07ece341616ebc9a6a5c58c401bd",
+          "url": "https://github.com/NatLabRockies/reVRt/commit/7ae322fc2b8886841cf8f8f3abe0a9bd1e034cea"
+        },
+        "date": 1776119438498,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "constant_cost",
+            "value": 68576370,
+            "range": "± 1287220",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "random_cost",
+            "value": 82925570,
+            "range": "± 2910665",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "multiple_near_routes",
+            "value": 100529404,
+            "range": "± 1756847",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "multiple_spread_routes",
+            "value": 147288922,
+            "range": "± 3393368",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "single_chunk",
+            "value": 603894159,
+            "range": "± 12817300",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "distance/0",
+            "value": 148843578,
+            "range": "± 1449377",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "distance/1",
+            "value": 154576613,
+            "range": "± 2354282",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "distance/2",
+            "value": 166501267,
+            "range": "± 1530635",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "distance/5",
+            "value": 208870787,
+            "range": "± 2430591",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "distance/10",
+            "value": 365499704,
+            "range": "± 6816177",
             "unit": "ns/iter"
           }
         ]
