aadc274e56a5a59d7164f711e9856eab8c25a2d4
Paul <pinchukpaul@gmail.com>
2026-05-19 01:11:52 -0600

    Minor docstring

--- revrt/utilities/handlers.py
+++ revrt/utilities/handlers.py
@@ -262,7 +262,8 @@ class LayeredFile:
         Returns
         -------
         dict
-            _description_
+            Dictionary of attribute metadata for the requested layer
+            as stored in the Zarr variable attrs.
 
         Raises
         ------
